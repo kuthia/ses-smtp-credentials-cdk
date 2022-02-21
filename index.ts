688110dd78d11bf7e3393274e799343990703371
Yatmak <yatmak@wncoms.com>
2022-02-21 19:50:00 +0900

    aws cdk version update

--- index.ts
+++ index.ts
@@ -1,54 +1,41 @@
-import * as cfn from '@aws-cdk/aws-cloudformation';
-import * as iam from '@aws-cdk/aws-iam';
-import * as lambdaNodejs from '@aws-cdk/aws-lambda-nodejs';
-import * as lambda from '@aws-cdk/aws-lambda';
-import * as cdk from '@aws-cdk/core';
-import * as customResource from '@aws-cdk/custom-resources';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as lambdaNodejs from 'aws-cdk-lib/aws-lambda-nodejs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as customResource from 'aws-cdk-lib/custom-resources';
 import * as path from 'path';
+import { CustomResource, Duration, Stack } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
 
-export class SesSmtpCredentialsProvider extends cdk.Construct {
+export class SesSmtpCredentialsProvider extends Construct {
     public readonly provider: customResource.Provider;
 
-    public static getOrCreate(scope: cdk.Construct): customResource.Provider {
-        const stack = cdk.Stack.of(scope);
+    public static getOrCreate(scope: Construct): customResource.Provider {
+        const stack = Stack.of(scope);
         const id = 'com.isotoma.cdk.custom-resources.ses-smtp-credentials';
         const x = (stack.node.tryFindChild(id) as SesSmtpCredentialsProvider) || new SesSmtpCredentialsProvider(stack, id);
         return x.provider;
     }
-    
-    constructor(scope: cdk.Construct, id: string) {
+
+    constructor(scope: Construct, id: string) {
         super(scope, id);
         this.provider = new customResource.Provider(this, 'ses-smtp-credentials-provider', {
             onEventHandler: new lambdaNodejs.NodejsFunction(this, 'ses-smtp-credentials-event', {
                 entry: path.join(__dirname, 'provider', 'main.ts'),
                 projectRoot: path.join(__dirname, 'provider'),
                 depsLockFilePath: path.join(__dirname, 'provider', 'package-lock.json'),
-                runtime: lambda.Runtime.NODEJS_12_X,
-                // To handle parcel-based versions of NodejsFunction
-                nodeModules: [
-                    'utf8',
-                    'aws-sdk',
-                ],
-                // To handle esbuild-based versions of NodejsFunction
+                runtime: lambda.Runtime.NODEJS_14_X,
                 bundling: {
-                    externalModules: [
-                        'utf8',
-                        'aws-sdk',
-                    ],
+                    // To handle parcel-based versions of NodejsFunction
+                    externalModules: ['utf8', 'aws-sdk'],
+                    // To handle esbuild-based versions of NodejsFunction
+                    nodeModules: ['utf8', 'aws-sdk'],
                 },
                 handler: 'onEvent',
-                timeout: cdk.Duration.minutes(5),
+                timeout: Duration.minutes(5),
                 initialPolicy: [
                     new iam.PolicyStatement({
                         resources: ['*'],
-                        actions: [
-                            'iam:CreateUser',
-                            'iam:PutUserPolicy',
-                            'iam:CreateAccessKey',
-                            'iam:DeleteUser',
-                            'iam:DeleteUserPolicy',
-                            'iam:DeleteAccessKey'
-                        ],
+                        actions: ['iam:CreateUser', 'iam:PutUserPolicy', 'iam:CreateAccessKey', 'iam:DeleteUser', 'iam:DeleteUserPolicy', 'iam:DeleteAccessKey'],
                     }),
                 ],
             } as lambdaNodejs.NodejsFunctionProps),
@@ -60,30 +47,30 @@ export class SesSmtpCredentialsProps {
     readonly region: string;
 }
 
-export class SesSmtpCredentials extends cdk.Construct {
+export class SesSmtpCredentials extends Construct {
     public readonly region: string;
-    private resource: cfn.CustomResource;
+    private resource: CustomResource;
 
-    constructor(scope: cdk.Construct, id: string, props: SesSmtpCredentialsProps) {
+    constructor(scope: Construct, id: string, props: SesSmtpCredentialsProps) {
         super(scope, id);
         if (!props.region) {
             throw new Error('No region specified');
         }
         this.region = props.region;
-        this.resource = new cfn.CustomResource(this, 'Resource', {
-            provider: SesSmtpCredentialsProvider.getOrCreate(this),
+        this.resource = new CustomResource(this, 'Resource', {
+            serviceToken: SesSmtpCredentialsProvider.getOrCreate(this).serviceToken,
             resourceType: 'Custom::SesSmtpCredentials',
             properties: {
-                Region: this.region
-            }
+                Region: this.region,
+            },
         });
     }
 
     public username(): string {
-        return this.resource.getAttString('Username')
+        return this.resource.getAttString('Username');
     }
-    
+
     public password(): string {
-        return this.resource.getAttString('Password')
+        return this.resource.getAttString('Password');
     }
 }
